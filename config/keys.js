34ba12e2942741ebe76a98cc104f62075e6d3663
Lihuor <lihuor@gmail.com>
2020-09-06 17:15:20 +1000

    Log in/Register Created

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword = 'mongodb+srv://dbUser:dbUser@123@cluster0.gnrp9.mongodb.net/<dbname>?retryWrites=true&w=majority';
+
+module.exports = {
+    mongoURI: dbPassword
+};
--- db/db.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE passport_demo;
-USE passport_demo;
-CREATE DATABASE Trading_DB;
-USE Trading_DB;
